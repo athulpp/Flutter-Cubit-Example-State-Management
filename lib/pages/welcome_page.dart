c75d4c613a93da0ca4c80dbbb0ccf9fbf79996b2
athulppnext <108659242+athulppnext@users.noreply.github.com>
2022-09-11 15:37:11 +0530

    welcome screen created

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = ["welcome-one.png", "welcome-two.png", "welcome-three.png"];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: 3,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.maxFinite,
+              height: double.maxFinite,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                        "img/" + images[index],
+                      ),
+                      fit: BoxFit.cover)),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(children: [Column(children: [],)],),
+
+              ),
+            );
+          }),
+    );
+  }
+}
